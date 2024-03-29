299d4f15af2fc884d96d3226f3edad948fcd26ff
Alan Romero <alan.romero1@yahoo.com.ar>
2024-03-28 22:31:57 -0300

    orders

--- src/components/pages/userOrders/UserOrders.jsx
+++ src/components/pages/userOrders/UserOrders.jsx
@@ -1,48 +1,50 @@
-import { collection, getDocs, query, where } from "firebase/firestore"
-import { useContext, useEffect, useState } from "react"
-import { db } from "../../../firebaseConfig"
-import { AuthContext } from "../../../context/AuthContext"
-
-
-
-
+import { useContext, useEffect } from "react";
+import { useState } from "react";
+import { db } from "../../../firebaseConfig";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { AuthContext } from "../../../context/AuthContext";
 
 const UserOrders = () => {
-    const [myOrders, setMyOrders] = useState([])
-    const {user} = useContext(AuthContext)
+  const [myOrders, setMyOrders] = useState([]);
+  const { user } = useContext(AuthContext);
 
-    useEffect(()=>{
-        const ordersCollections = collection(db, "orders")
-        let ordersFiltered = query(ordersCollections, where("email", "==", user.email))
-        getDocs(ordersFiltered).then(res =>{
-            const newArr = res.docs.map(order =>{
-                return {...order.data(), id: order.id}
-            })
-            setMyOrders(newArr)
-        }).catch(error => console.log(error))
-    },[user.email])
+  useEffect(() => {
+    const ordersCollections = collection(db, "orders");
+    let ordersFiltered = query(
+      ordersCollections,
+      where("email", "==", user.email)
+    );
+    getDocs(ordersFiltered)
+      .then((res) => {
+        const newArr = res.docs.map((order) => {
+          return { ...order.data(), id: order.id };
+        });
+        setMyOrders(newArr);
+      })
+      .catch((error) => console.log(error));
+  }, [user.email]);
 
-    return (
+  console.log(myOrders);
+  return (
     <div>
-        <h1> estoy en mis compras</h1>
-        {
-            myOrders.map(order =>{
-                return <div key={order.id} style={{border:"2px solid black"}}>
-                    {
-                        order?.items?.map(product=>{
-                            return <div key={product.id}>
-                                <h2>{product.title}</h2>
-                                <h3>{product.quantity}</h3>   
-                            </div>
-                        })
-                    }
-                    <h4>el total de la orden es {order.total}</h4>
-
+      <h1>estoy en mis ordenes</h1>
+      {myOrders.map((order) => {
+        return (
+          <div key={order.id} style={{ border: "2px solid black" }}>
+            {order?.items?.map((product) => {
+              return (
+                <div key={product.id}>
+                  <h2>{product.title}</h2>
+                  <h3>{product.quantity}</h3>
                 </div>
-            })
-        }
+              );
+            })}
+            <h4>El total de la orden es {order.total}</h4>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default UserOrders
+export default UserOrders;
